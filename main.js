961b49e0417b3c9677b63c65be3b7f200208b873
kreit <reitenbaughkm@gmail.com>
2024-11-05 01:15:51 -0600

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const span = document.querySelector('.mess-with-me')
+span.style.fontSize = '40px'
+
+const paragraph = document.querySelector('p.mess-with-me')
+paragraph.style.backgroundColor = 'green'
+
+const image = document.querySelector('#hide-me')
+image.style.visibility = 'hidden'
+
+const images = document.querySelector('#triceratops')
+images.style.width = '324px'
+
+const section = document.querySelector('#hide-me-area')
+section.style.display = 'none'
+
 
