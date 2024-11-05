a025c4a8e39d4ddc86690cab68bd3346625dad80
kreit <reitenbaughkm@gmail.com>
2024-11-05 08:47:35 -0600

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -7,10 +7,41 @@ paragraph.style.backgroundColor = 'green'
 const image = document.querySelector('#hide-me')
 image.style.visibility = 'hidden'
 
-const images = document.querySelector('#triceratops')
-images.style.width = '324px'
-
 const section = document.querySelector('#hide-me-area')
 section.style.display = 'none'
 
-document.querySelector('.mess-with-me')
+const images = document.querySelector('#triceratops')
+images.style.width = '324px'
+
+document.querySelector('.mess-with-me').addEventListener("click", function(){
+     span.style.color = 'orange'
+})
+
+const picture = document.getElementById('triceratops')
+
+picture.addEventListener("click", function(){
+    picture.style.border = '5px solid red'
+})
+
+const dinosaur = document.getElementById('feathers')
+
+dinosaur.addEventListener("click", function(){
+    dinosaur.style.opacity = '50%'
+})
+
+const button = document.getElementById('toggle')
+const sections = document.getElementById('row')
+
+button.addEventListener("click", function(){
+    sections.style.backgroundColor = 'aquamarine'
+})
+
+const photos = document.getElementById('biggify')
+
+photos.addEventListener("mouseover", function(){
+    photos.style.width = '200px'
+})
+
+photos.addEventListener("mouseleave", function(){
+    photos.style.width = '162px'
+})
